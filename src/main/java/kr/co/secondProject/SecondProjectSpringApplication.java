9e167a1c8fb2dd680e495392e25286b720ed4686
hongsoonchan02 <asm640789@gmail.com>
2026-03-20 18:14:00 +0900

    spring 초기 세팅

---
+++ src/main/java/kr/co/secondProject/SecondProjectSpringApplication.java
@@ -0,0 +1,13 @@
+package kr.co.secondProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecondProjectSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecondProjectSpringApplication.class, args);
+	}
+
+}
