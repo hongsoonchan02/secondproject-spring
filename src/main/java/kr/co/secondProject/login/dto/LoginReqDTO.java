24785e467f487e67c8d8908cd8c2b5d61ef5e0c2
beanCan <bean.can0325@gmail.com>
2026-03-24 13:35:32 +0900

    feat(dto): Swagger @Schema 어노테이션 추가

--- src/main/java/kr/co/secondProject/login/dto/LoginReqDTO.java
+++ src/main/java/kr/co/secondProject/login/dto/LoginReqDTO.java
@@ -1,13 +1,18 @@
 package kr.co.secondProject.login.dto;
 
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Getter
 @NoArgsConstructor
+@Schema(description="로그인 요청 DTO")
 public class LoginReqDTO {
 
-    private String email;      //로그인할 이메일
-    private String password;   //비밀번호
+    @Schema(description = "이메일", example = "sojeong@test.com")
+    private String email;
+
+    @Schema(description = "비밀번호", example = "1234")
+    private String password;
 }
--- src/main/java/kr/co/secondProject/login/dto/LoginResDTO.java
+++ src/main/java/kr/co/secondProject/login/dto/LoginResDTO.java
@@ -1,17 +1,28 @@
 package kr.co.secondProject.login.dto;
 
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 @Getter
 @AllArgsConstructor
+@Schema(description = "로그인 응답 DTO")
 public class LoginResDTO {
 
-    private Long id;      //유저넘버
-    private String name;  //이름
-    private String email; //이메일
-    private String role;  //권한 (ADMIN/ MANAGER/ EMPLOYEE)
-    private String position;//직책
+    @Schema(description = "유저넘버(유저id)")
+    private Long id;
+
+    @Schema(description = "이름")
+    private String name;
+
+    @Schema(description = "이메일")
+    private String email;
+
+    @Schema(description = "권한")
+    private String role;
+
+    @Schema(description = "직책")
+    private String position;
 
 }
