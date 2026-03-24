71f7cb3ce6ec1d507f21b4edde5c9c960b05a9e7
beanCan <bean.can0325@gmail.com>
2026-03-24 10:20:46 +0900

    feat(cpnfig): securityConfig 추가

--- src/main/java/kr/co/secondProject/login/repository/EmailVerificationRepository.java
+++ src/main/java/kr/co/secondProject/login/repository/EmailVerificationRepository.java
@@ -1,5 +1,6 @@
 package kr.co.secondProject.login.repository;
 
+import kr.co.secondProject.login.entity.EmailVerification;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface EmailVerificationRepository extends JpaRepository<EmailVerification, Long> {
