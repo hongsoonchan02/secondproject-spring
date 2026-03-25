dd6d6edf6dc77e8f3bb65a2ccd248e9010e55b86
beanCan <bean.can0325@gmail.com>
2026-03-25 17:08:10 +0900

    feat(entity): Attendance Builder 추가 및 checkOut 메서드 추가

--- src/main/java/kr/co/secondProject/login/entity/Attendance.java
+++ src/main/java/kr/co/secondProject/login/entity/Attendance.java
@@ -22,14 +22,17 @@ public class Attendance {
     private Employee employee;      // 유저넘버(FK) :직원ID
 
     private LocalDateTime date;     // 날짜
-
     private LocalDateTime startTime;// 출근시간
-
     private LocalDateTime endTime;  // 퇴근시간
-
     private String allTime;         // 근무시간
-
     private String state;           // 근태상태
 
-
+    //퇴근 처리 메서드 <- 추가
+    //this -> 지금 이 객체 자신을 가리킴
+    //퇴근시각, 근무시간, 상태를 업데이트
+    public void checkOut(LocalDateTime endTime, String allTime, String state){
+        this.endTime =endTime;
+        this.allTime = allTime;
+        this.state =state;
+    }
 }
