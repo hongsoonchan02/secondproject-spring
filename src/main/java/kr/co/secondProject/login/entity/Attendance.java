8fb4848f52623ba6ed8a2fd1a62419dc19d0bde6
beanCan <bean.can0325@gmail.com>
2026-03-27 11:49:16 +0900

    feat(entity): Attendance Builder 추가 및 checkOut 메서드 추가

--- src/main/java/kr/co/secondProject/login/entity/Attendance.java
+++ src/main/java/kr/co/secondProject/login/entity/Attendance.java
@@ -2,15 +2,16 @@ package kr.co.secondProject.login.entity;
 
 
 import jakarta.persistence.*;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
 import java.time.LocalDateTime;
 
 @Entity
 @Getter
 @NoArgsConstructor
+@Builder
 @Table(name = "attendance")
+@AllArgsConstructor
 public class Attendance {
 
     @Id
@@ -30,6 +31,16 @@ public class Attendance {
     private String allTime;         // 근무시간
 
     private String state;           // 근태상태
+
+
+    //퇴근처리 메서드
+    //this -> 지금 이 객체 자신을 가리킴
+    // 퇴근시각, 근무시간, 상태를 업그레이드
+    public void checkOut(LocalDateTime endTime, String allTime, String state){
+        this.endTime = endTime;
+        this.allTime = allTime;
+        this.state = state;
+    }
 
 
 }
