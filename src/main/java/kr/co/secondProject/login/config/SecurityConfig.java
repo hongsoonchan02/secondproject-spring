da0d4f5be212b3e94ab2482bf4f2eefb61109602
beanCan <bean.can0325@gmail.com>
2026-03-31 12:04:52 +0900

    feat(login): SecurityConfig CORS 설정, AttendanceRepository count 추가

--- src/main/java/kr/co/secondProject/login/config/SecurityConfig.java
+++ src/main/java/kr/co/secondProject/login/config/SecurityConfig.java
@@ -18,7 +18,6 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
-                .cors(cors -> cors.configurationSource(corsConfigurationSource()))
                 .csrf(csrf -> csrf.disable())
                 .authorizeHttpRequests(auth -> auth
                         .anyRequest().permitAll()
