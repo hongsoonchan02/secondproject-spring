e599170e49e0f41e9084f620a18782bb22c95669
beanCan <bean.can0325@gmail.com>
2026-04-01 16:51:43 +0900

    fix: DepartmentRepository findTop5 메서드명 대소문자 오류 수정

--- src/main/java/kr/co/secondProject/department/repository/DepartmentRepository.java
+++ src/main/java/kr/co/secondProject/department/repository/DepartmentRepository.java
@@ -11,7 +11,7 @@ public interface DepartmentRepository extends JpaRepository<Department, Long> {
 
 	List<Department> findByDpNameContaining(String keyword);
 	
-	List<Department> findTop5ByorderByIdDesc(); // 최근 등록된(id가 높은 순) 부서를 가져오는 코드
+	List<Department> findTop5ByOrderByIdDesc(); // 최근 등록된(id가 높은 순) 부서를 가져오는 코드
 														  // 없을수도 있기에 Optional 처리
 	
 	
