0ef23bdd750996e8ff4db2aac6c4e5541e866237
hyun <syjk7503@gmail.com>
2026-04-01 09:58:35 +0900

    style(controller): 코드 스타일 변경

--- src/main/java/kr/co/secondProject/vacation/controller/VacationController.java
+++ src/main/java/kr/co/secondProject/vacation/controller/VacationController.java
@@ -1,14 +1,17 @@
 package kr.co.secondProject.vacation.controller;
 
-import java.util.List;
-
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.servlet.http.HttpSession;
 import kr.co.secondProject.vacation.dto.ResVacationDTO;
 import kr.co.secondProject.vacation.service.VacationService;
 import lombok.RequiredArgsConstructor;
@@ -20,13 +23,21 @@ import lombok.RequiredArgsConstructor;
 public class VacationController {
 	private final VacationService vacationService;
 	
-	// [GET] 특정 직원 휴가 신청 전체 조회
-	@GetMapping("/{employeeId")
-	public ResponseEntity<List<ResVacationDTO>> getVacationList(
-			@PathVariable Long employeeId){
-		return ResponseEntity.ok(vacationService.getVacationList(employeeId));
-	}
 	
+	// 휴가 관리 신청 내역
+	@GetMapping("/my-history")
+    public ResponseEntity<Page<ResVacationDTO>> getMyHistory(
+            @RequestParam(defaultValue = "0") int page,
+            @RequestParam(defaultValue = "5") int size,
+            HttpSession session) {
+
+        Long loginId = getLoginId(session);
+        if (loginId == null) return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
+
+        Pageable pageable = PageRequest.of(page, size);
+        return ResponseEntity.ok(vacationService.getMyVacationHistory(loginId, pageable));
+    }
+
 	
 
 }
